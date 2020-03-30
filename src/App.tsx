649a4b5f5fb74814873d549250be46a26e9ba7c6
Andreas jantschnig <andreas.jantschnig@gmail.com>
2020-03-30 11:48:41 +0200

    add GA

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
 import React from "react";
+import ReactGA from "react-ga";
 
 import AppTheme from "./Theme";
 import Page from "./components/Page";
 
+const { NODE_ENV } = process.env;
+
 class App extends React.Component {
+  constructor(props: {}) {
+    super(props);
+    let gaTrackingId = "UA-000000-01";
+    if (NODE_ENV === "production") {
+      gaTrackingId = "UA-162148086-1";
+    }
+    ReactGA.initialize(gaTrackingId);
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }
+
   render = () => {
     return (
       <AppTheme>
