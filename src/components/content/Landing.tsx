c46a63fb67c6a895ee38c67242e5e792c70b1eef
Andreas jantschnig <andreas.jantschnig@gmail.com>
2020-03-30 20:55:27 +0200

    updated landing page content

--- src/components/content/Landing.tsx
+++ src/components/content/Landing.tsx
@@ -1,12 +1,15 @@
 import React from "react";
-import { Typography } from "@material-ui/core";
+import { Box, Typography, Grid } from "@material-ui/core";
+import styled, { AnyStyledComponent } from "styled-components";
 
 import Initiative from "./Initiative";
-import Partners from "./Partners";
 import Faq from "./Faq";
 
+import LandingPartnersImage from "../../images/landing_partners.png";
+import BecomePart from "../common/BecomePart";
+
 interface ILandingProps {
-  lang: string;
+  lang: "de" | "en";
 }
 
 class Landing extends React.PureComponent<ILandingProps, {}> {
@@ -14,12 +17,34 @@ class Landing extends React.PureComponent<ILandingProps, {}> {
     return (
       <>
         <Initiative lang={this.props.lang}></Initiative>
-        <Typography variant="h2">Founding & Tech Partner</Typography>
-        <Partners lang={this.props.lang}></Partners>
+        <Box id="partners" paddingBottom={4} marginTop={4}>
+          <Grid container>
+            <Grid item xs={12} sm={4}>
+              <Typography variant="h2">Founding & Tech Partner</Typography>
+              <Typography variant="body1">
+                Zusammen stellen wir Ressourcen zur Verfuegung um Digitale Tools
+                im Kampf gg Covid und anderen Epidemien zu unterstuetzen.
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={8}>
+              <LandingPartners
+                src={LandingPartnersImage}
+                alt="Partners"
+              ></LandingPartners>
+            </Grid>
+          </Grid>
+        </Box>
+        <Box paddingBottom={4}>
+          <BecomePart lang={this.props.lang}></BecomePart>
+        </Box>
         <Faq lang={this.props.lang}></Faq>
       </>
     );
   };
 }
+
+const LandingPartners: AnyStyledComponent = styled.img`
+  width: 100%;
+`;
 
 export default Landing;
