07324b9bd3d6423c5b29546ad25c7f39a3009af8
Sebastian Weikart <sebastian.weikart@finleap.com>
2020-03-31 14:09:34 +0200

    add download press kit

--- src/components/content/Press.tsx
+++ src/components/content/Press.tsx
@@ -3,10 +3,11 @@ import { Box, Typography, Avatar, Grid, Link } from "@material-ui/core";
 import styled, { AnyStyledComponent } from "styled-components";
 import { translated } from "../../util";
 import PressContactPhoto from "../../images/solveigh_rathenow.png";
-import Logo from "../../../images/logo.svg";
+import Logo from "../../images/logo.svg";
 import BecomePart from "../common/BecomePart";
 import EmailIcon from "@material-ui/icons/Email";
 import PhoneIcon from "@material-ui/icons/Phone";
+import GetAppIcon from "@material-ui/icons/GetApp";
 interface IPressProps {
   lang: "de" | "en";
 }
@@ -57,18 +58,34 @@ class PressContact extends React.PureComponent<IPressProps, {}> {
           </Grid>
         </Grid>
         <hr></hr>
-        <Typography variant="h2">Resources</Typography>
-        <Typography variant="body1">
-          Download the GesundZusammen logo in various formats.
-        </Typography>
-        <Link to="/" style={{ float: "left" }}>
-          <DownloadLogo src={Logo} alt="#GesundZusammen"></DownloadLogo>
-        </Link>
-        <Typography variant="body1">
-          Includes EPS, JPG, and PNG formats.
-        </Typography>
-        <Typography variant="body1">For digital and print.</Typography>
-        <hr></hr>
+        <Box paddingBottom={4} marginTop={4}>
+          <Typography variant="h2">
+            {translated(this.props.lang).press.resources}
+          </Typography>
+          <Typography variant="body1">
+            {translated(this.props.lang).press.resourcesLine1}
+          </Typography>
+          <Typography variant="body1">
+            <Link href="/PressKit.zip">
+              <DownloadLogo src={Logo} alt="#GesundZusammen"></DownloadLogo>
+            </Link>
+          </Typography>
+          <Typography variant="body1">
+            {translated(this.props.lang).press.resourcesLine2}
+          </Typography>
+          <Grid container direction="row" alignItems="center">
+            <Grid item>
+              <StyledGetAppIcon />
+            </Grid>
+            <Grid item>
+              <Typography variant="body1">
+                <Link href="/PressKit.zip">
+                  {translated(this.props.lang).press.downloadLogo}
+                </Link>
+              </Typography>
+            </Grid>
+          </Grid>
+        </Box>
         <Box paddingBottom={4} marginTop={4}>
           <BecomePart lang={this.props.lang}></BecomePart>
         </Box>
@@ -99,11 +116,18 @@ const StyledPhoneIcon: AnyStyledComponent = styled(PhoneIcon)`
   padding-right: 1rem;
 `;
 
+const StyledGetAppIcon: AnyStyledComponent = styled(GetAppIcon)`
+  color: #0a6eaa;
+  padding-right: 1rem;
+`;
+
 const DownloadLogo: AnyStyledComponent = styled.img`
   display: block;
   margin: 0 auto;
   background-color: #003269;
   border-radius: 4px;
+  width: 368px;
+  height: 200px;
   @media (min-width: 600px) {
     margin: 0;
     float: left;
