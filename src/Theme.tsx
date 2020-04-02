a52327ee0deaf1cbd19df3c047bcd07e0f870393
Andreas jantschnig <andreas.jantschnig@gmail.com>
2020-04-02 09:23:29 +0200

    style update initiatives page

--- src/Theme.tsx
+++ src/Theme.tsx
@@ -91,13 +91,13 @@ const theme = responsiveFontSizes(
         fontWeight: 600,
       },
       subtitle1: {
-        fontSize: "0.7rem",
+        fontSize: "0.8rem",
         fontWeight: "normal",
         textTransform: "uppercase",
         color: "#003269",
       },
       subtitle2: {
-        fontSize: "0.6rem",
+        fontSize: "0.8rem",
         fontWeight: "normal",
         textTransform: "uppercase",
         color: "#3C3C3C",
