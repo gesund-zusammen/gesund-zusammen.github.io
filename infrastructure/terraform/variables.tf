ff23d0126733ff975930a782839291ae960c392d
Sebastian Weikart <sebastian.weikart@finleap.com>
2020-04-07 15:32:19 +0200

    adding new variables for staging and production differentiation

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -18,22 +18,42 @@ variable "aws-secret-key" {
   type = string
 }
 
-variable "bucket_name" {
+variable "bucket_name_staging" {
   description = "name of the S3 bucket where the static website will be hosted, e.g. \"demo.com\""
   type        = string
 }
 
-variable "domain_name" {
+variable "bucket_name_production" {
+  description = "name of the S3 bucket where the static website will be hosted, e.g. \"demo.com\""
+  type        = string
+}
+
+variable "domain_name_staging" {
   description = "domain for the cloudfront distribution e.g. \"demo.com\""
   type        = string
 }
 
-variable "zone_id" {
+variable "domain_name_production" {
+  description = "domain for the cloudfront distribution e.g. \"demo.com\""
+  type        = string
+}
+
+variable "zone_id_staging" {
   description = "id of the route53 zone"
   type        = string
 }
 
-variable "cert_arn" {
+variable "cert_arn_staging" {
+  description = "The ARN of the certificate that the CloudFront distribution uses for https"
+  type        = string
+}
+
+variable "zone_id_production" {
+  description = "id of the route53 zone"
+  type        = string
+}
+
+variable "cert_arn_production" {
   description = "The ARN of the certificate that the CloudFront distribution uses for https"
   type        = string
 }
