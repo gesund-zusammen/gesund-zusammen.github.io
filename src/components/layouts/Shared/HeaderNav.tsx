b0049e590d98091af3fb4333fa1a765f004c510f
Andreas jantschnig <andreas.jantschnig@gmail.com>
2020-04-07 15:47:54 +0200

    add mobile header nav

--- src/components/layouts/Shared/HeaderNav.tsx
+++ src/components/layouts/Shared/HeaderNav.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { Switch } from "@material-ui/core";
+import { Switch, IconButton, Menu, MenuItem, Hidden } from "@material-ui/core";
+import { Menu as MenuIcon } from "@material-ui/icons";
 import styled, { css, AnyStyledComponent } from "styled-components";
 import { withRouter, RouteComponentProps } from "react-router";
 import { Link } from "react-router-dom";
@@ -9,7 +10,28 @@ import Logo from "../../../images/logo.svg";
 
 interface IHeaderNavProps extends RouteComponentProps, WithTranslation {}
 
-class HeaderNav extends React.Component<IHeaderNavProps, {}> {
+interface IHeaderNavState {
+  menuAnchor: null | Element;
+}
+
+const DEFAULT_STATE: IHeaderNavState = {
+  menuAnchor: null,
+};
+
+class HeaderNav extends React.Component<IHeaderNavProps, IHeaderNavState> {
+  constructor(props: IHeaderNavProps) {
+    super(props);
+    this.state = DEFAULT_STATE;
+  }
+
+  handleMenuIconClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    this.setState({ menuAnchor: event.currentTarget });
+  };
+
+  handleMenuClose = () => {
+    this.setState({ menuAnchor: null });
+  };
+
   handleLangChange = () => {
     const newLang = this.props.i18n.language === "de" ? "en" : "de";
     this.props.i18n.changeLanguage(newLang, () => {
@@ -25,32 +47,75 @@ class HeaderNav extends React.Component<IHeaderNavProps, {}> {
         <Link to={`/${this.props.i18n.language}/`}>
           <HeaderLogo src={Logo} alt="#GesundZusammen" />
         </Link>
-        <HeaderNavItems>
-          <HeaderNavLinkItem to={`/${this.props.i18n.language}/program`}>
-            {this.props.t("program.header.title")}
-          </HeaderNavLinkItem>
-          <HeaderNavLinkItem to={`/${this.props.i18n.language}/database`}>
-            {this.props.t("header.nav.initiatives")}
-          </HeaderNavLinkItem>
-          <HeaderNavLinkItem to={`/${this.props.i18n.language}/partners`}>
-            {this.props.t("header.nav.supporters")}
-          </HeaderNavLinkItem>
-          <HeaderNavLinkItem to={`/${this.props.i18n.language}/faq`}>
-            {this.props.t("header.nav.faqs")}
-          </HeaderNavLinkItem>
-          <HeaderLangSwitch>
-            <StyledSwitch
-              checked={this.props.i18n.language === "de"}
-              onChange={this.handleLangChange}
-              disableRipple={true}
-              color="primary"
-              name="langSwitch"
-            />
-            <LangDisplay>
-              {this.props.i18n.language === "de" ? "en" : "de"}
-            </LangDisplay>
-          </HeaderLangSwitch>
-        </HeaderNavItems>
+
+        <HeaderLangSwitch>
+          <StyledSwitch
+            checked={this.props.i18n.language === "de"}
+            onChange={this.handleLangChange}
+            disableRipple={true}
+            color="primary"
+            name="langSwitch"
+          />
+          <LangDisplay>
+            {this.props.i18n.language === "de" ? "en" : "de"}
+          </LangDisplay>
+        </HeaderLangSwitch>
+
+        <Hidden smUp>
+          <MenuToggleButton
+            edge="end"
+            color="inherit"
+            aria-label="main-menu"
+            onClick={this.handleMenuIconClick}
+          >
+            <MenuIcon />
+          </MenuToggleButton>
+          <HeaderNavMenu
+            id="main-menu"
+            anchorEl={this.state.menuAnchor}
+            open={Boolean(this.state.menuAnchor)}
+            onClose={this.handleMenuClose}
+            keepMounted
+          >
+            <MenuItem onClick={this.handleMenuClose}>
+              <HeaderNavLinkItem to={`/${this.props.i18n.language}/program`}>
+                {this.props.t("program.header.title")}
+              </HeaderNavLinkItem>
+            </MenuItem>
+            <MenuItem onClick={this.handleMenuClose}>
+              <HeaderNavLinkItem to={`/${this.props.i18n.language}/database`}>
+                {this.props.t("header.nav.initiatives")}
+              </HeaderNavLinkItem>
+            </MenuItem>
+            <MenuItem onClick={this.handleMenuClose}>
+              <HeaderNavLinkItem to={`/${this.props.i18n.language}/partners`}>
+                {this.props.t("header.nav.supporters")}
+              </HeaderNavLinkItem>
+            </MenuItem>
+            <MenuItem onClick={this.handleMenuClose}>
+              <HeaderNavLinkItem to={`/${this.props.i18n.language}/faq`}>
+                {this.props.t("header.nav.faqs")}
+              </HeaderNavLinkItem>
+            </MenuItem>
+          </HeaderNavMenu>
+        </Hidden>
+
+        <Hidden smDown>
+          <HeaderNavItems>
+            <HeaderNavLinkItem to={`/${this.props.i18n.language}/program`}>
+              {this.props.t("program.header.title")}
+            </HeaderNavLinkItem>
+            <HeaderNavLinkItem to={`/${this.props.i18n.language}/database`}>
+              {this.props.t("header.nav.initiatives")}
+            </HeaderNavLinkItem>
+            <HeaderNavLinkItem to={`/${this.props.i18n.language}/partners`}>
+              {this.props.t("header.nav.supporters")}
+            </HeaderNavLinkItem>
+            <HeaderNavLinkItem to={`/${this.props.i18n.language}/faq`}>
+              {this.props.t("header.nav.faqs")}
+            </HeaderNavLinkItem>
+          </HeaderNavItems>
+        </Hidden>
       </>
     );
   };
@@ -63,6 +128,28 @@ const HeaderLogo: AnyStyledComponent = styled.img`
   @media (min-width: 600px) {
     margin: 0;
     float: left;
+  }
+`;
+
+const MenuToggleButton: AnyStyledComponent = styled(IconButton)`
+  && {
+    float: left;
+
+    @media (min-width: 600px) {
+      float: right;
+    }
+  }
+`;
+
+const HeaderNavMenu: AnyStyledComponent = styled(Menu)`
+  @media (min-width: 600px) {
+    display: inline-block;
+    width: 100%;
+    margin-top: 1rem;
+    float: right;
+    width: auto;
+    margin-top: 0;
+    text-align: right;
   }
 `;
 
@@ -81,16 +168,17 @@ const HeaderNavItems: AnyStyledComponent = styled.div`
 `;
 
 const headerNavItemStyles = css`
+  color: #00aac8;
   display: inline-block;
   width: 50%;
   font-family: inherit;
-  color: #ffffff;
   text-decoration: none;
   font-size: 1.1rem;
   font-weight: 600;
   line-height: 3rem;
 
   @media (min-width: 600px) {
+    color: #ffffff;
     display: inline-block;
     width: auto;
     margin-left: 2rem;
@@ -106,12 +194,11 @@ const HeaderNavLinkItem: AnyStyledComponent = styled(Link)`
 `;
 
 const HeaderLangSwitch: AnyStyledComponent = styled.div`
-  display: inline-block;
-  width: 100%;
+  float: right;
 
   @media (min-width: 600px) {
-    width: auto;
     margin-left: 1.3rem;
+    margin-top: 4px;
   }
 `;
 
