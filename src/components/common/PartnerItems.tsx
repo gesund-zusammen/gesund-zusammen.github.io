c971eca065ffe2c7f52f8e4dd8353d6d3abc0e67
Andreas jantschnig <andreas.jantschnig@gmail.com>
2020-04-16 15:23:57 +0200

    remove Face from partner items

--- src/components/common/PartnerItems.tsx
+++ src/components/common/PartnerItems.tsx
@@ -52,50 +52,46 @@ class PartnerItems extends React.Component<IPartnerItemsProps, {}> {
   render = () => {
     return (
       <Box id="partners" paddingBottom={4} marginTop={4}>
-        <Fade right cascade>
-          {this.getCategories(this.props.categorySlug).map(
-            (category, index) => {
-              const categoryKey: string = "category-" + index;
-              return (
-                <div key={categoryKey}>
-                  <Typography variant="h2">
-                    {category.name[this.props.i18n.language]}
-                  </Typography>
-                  <Box paddingTop={4} paddingBottom={4}>
-                    <Grid
-                      container
-                      direction="row"
-                      justify="center"
-                      alignItems="center"
-                      spacing={4}
-                    >
-                      {this.getPartners(category.slug).map(
-                        (partner, partnerIndex) => {
-                          const partnerCardKey: string =
-                            "category-" + index + "-partner-" + partnerIndex;
-                          return (
-                            <Card
-                              key={partnerCardKey}
-                              name={partner.name}
-                              image={
-                                require(`../../images/partners/${partner.image}`)
-                                  .default
-                              }
-                              link={partner.link}
-                              color={partner.color}
-                              imageXL={partner.imageXL}
-                              nameColorInverted={partner.nameColorInverted}
-                            />
-                          );
-                        },
-                      )}
-                    </Grid>
-                  </Box>
-                </div>
-              );
-            },
-          )}
-        </Fade>
+        {this.getCategories(this.props.categorySlug).map((category, index) => {
+          const categoryKey: string = "category-" + index;
+          return (
+            <div key={categoryKey}>
+              <Typography variant="h2">
+                {category.name[this.props.i18n.language]}
+              </Typography>
+              <Box paddingTop={4} paddingBottom={4}>
+                <Grid
+                  container
+                  direction="row"
+                  justify="center"
+                  alignItems="center"
+                  spacing={4}
+                >
+                  {this.getPartners(category.slug).map(
+                    (partner, partnerIndex) => {
+                      const partnerCardKey: string =
+                        "category-" + index + "-partner-" + partnerIndex;
+                      return (
+                        <Card
+                          key={partnerCardKey}
+                          name={partner.name}
+                          image={
+                            require(`../../images/partners/${partner.image}`)
+                              .default
+                          }
+                          link={partner.link}
+                          color={partner.color}
+                          imageXL={partner.imageXL}
+                          nameColorInverted={partner.nameColorInverted}
+                        />
+                      );
+                    },
+                  )}
+                </Grid>
+              </Box>
+            </div>
+          );
+        })}
       </Box>
     );
   };
